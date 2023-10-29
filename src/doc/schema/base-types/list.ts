cba6e5c5f5b15320f5c8874261c782ccd3f9448d
0tickpulse <0tickpulse@gmail.com>
2023-10-29 18:41:13 +0800

    add schema list + mythicskill.skills

---
+++ src/doc/schema/base-types/list.ts
@@ -0,0 +1,79 @@
+import { isSeq } from "yaml";
+
+import type { ParsedNode } from "yaml";
+import type { MythicDoc, Workspace } from "../../../index.js";
+
+import { ValidationResult } from "../../../index.js";
+import { Schema } from "../schema.js";
+import { DIAGNOSTIC_DEFAULT } from "../../../errors.js";
+
+export class SchemaList extends Schema {
+    public constructor(public readonly items?: Schema | Schema[]) {
+        super();
+    }
+
+    public override toString(
+        ws: Workspace,
+        doc: MythicDoc,
+        value: ParsedNode,
+    ): string {
+        if (this.items === undefined) {
+            return "list";
+        }
+        if (Array.isArray(this.items)) {
+            return `[${this.items
+                .map((item) => item.toString(ws, doc, value))
+                .join(", ")}]`;
+        }
+        return `${this.items.toString(ws, doc, value)}[]`;
+    }
+
+    public override partialProcess(
+        ws: Workspace,
+        doc: MythicDoc,
+        value: ParsedNode,
+    ): ValidationResult {
+        if (!isSeq(value)) {
+            return new ValidationResult([
+                {
+                    ...DIAGNOSTIC_DEFAULT,
+                    message: `Expected ${this.toString(ws, doc, value)}.`,
+                    range: doc.convertToRange(value.range),
+                },
+            ]);
+        }
+        if (this.items === undefined) {
+            return new ValidationResult();
+        }
+        const result = new ValidationResult();
+        if (Array.isArray(this.items)) {
+            if (value.items.length > this.items.length) {
+                result.diagnostics.push({
+                    ...DIAGNOSTIC_DEFAULT,
+                    message: `Too many items in list. Expected ${this.items.length}, but got ${value.items.length}.`,
+                    range: doc.convertToRange(value.range),
+                });
+            }
+            if (value.items.length < this.items.length) {
+                result.diagnostics.push({
+                    ...DIAGNOSTIC_DEFAULT,
+                    message: `Too few items in list. Expected ${this.items.length}, but got ${value.items.length}.`,
+                    range: doc.convertToRange(value.range),
+                });
+            }
+            this.items.forEach((item, i) => {
+                const yamlItem = value.items[i];
+                if (!yamlItem) {
+                    return;
+                }
+                const itemResult = item.partialProcess(ws, doc, yamlItem);
+                result.merge(itemResult);
+            });
+        } else {
+            for (const item of value.items) {
+                result.merge(this.items.partialProcess(ws, doc, item));
+            }
+        }
+        return result;
+    }
+}
--- src/doc/schema/file-schemas/mythicSkill.ts
+++ src/doc/schema/file-schemas/mythicSkill.ts
@@ -1,7 +1,9 @@
 import { mdSeeAlso } from "../../../util/markdown.js";
+import { SchemaList } from "../base-types/list.js";
 import { SchemaMap } from "../base-types/map.js";
 import { SchemaNumber } from "../base-types/number.js";
 import { SchemaObject } from "../base-types/object.js";
+import { SchemaString } from "../base-types/string.js";
 
 export const MYTHIC_SKILL_SCHEMA = new SchemaMap(new SchemaObject({
     Cooldown: {
@@ -10,5 +12,12 @@ export const MYTHIC_SKILL_SCHEMA = new SchemaMap(new SchemaObject({
         description:
             "The cooldown of this skill in seconds."
             + mdSeeAlso("Skills/Metaskills#cooldown"),
+    },
+    Skills: {
+        schema: new SchemaList(new SchemaString()),
+        required: true,
+        description:
+            "The skills that this skill will use."
+            + mdSeeAlso("Skills/Metaskills#skills"),
     },
 }));
