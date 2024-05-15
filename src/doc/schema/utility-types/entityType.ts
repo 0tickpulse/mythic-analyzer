3c7b947369057a550e1c41eea7f13f453de41ff1
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2024-05-15 14:46:52 +0800

    use ignorecase include for entitytype utilitytype

--- src/doc/schema/utility-types/entityType.ts
+++ src/doc/schema/utility-types/entityType.ts
@@ -3,11 +3,12 @@ import { SemanticTokenTypes } from "vscode-languageserver";
 import { Highlight } from "../../../lsp/models/highlight.js";
 import { ENTITIES } from "../../../mythic/defaultData/entities.js";
 import { SchemaString } from "../base-types/string.js";
+import { includesIgnoreCase } from "../../../util/string.js";
 
 export const SCHEMA_ENTITY_TYPE = new SchemaString(ENTITIES)
     .withName("entity_type")
     .onPartialProcess((ws, doc, value, result) => {
-        if (!ENTITIES.has((value.toJSON() as string).toUpperCase())) {
+        if (!includesIgnoreCase(ENTITIES, value.toJSON() as string)) {
             return;
         }
         result.highlights.unshift(new Highlight(doc.convertToRange(value.range), SemanticTokenTypes.enumMember));
