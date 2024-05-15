00872563fbf4d4da03ed47e7bf8d21b3780ac448
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2024-05-15 14:47:04 +0800

    change declaration format

--- src/doc/schema/utils/component.ts
+++ src/doc/schema/utils/component.ts
@@ -1,13 +1,17 @@
 import { SemanticTokenTypes } from "vscode-languageserver";
 import { isMap } from "yaml";
 
-import type { ParsedNode } from "yaml";
+import type { Pair, ParsedNode } from "yaml";
 import type { MythicDoc, ValidationResult, Workspace } from "../../../index.js";
+import type { MythicComponentDeclaration } from "../../../document-models/mythiccomponent.js";
 
 import { DIAGNOSTIC_DEFAULT } from "../../../index.js";
 import { Highlight } from "../../../lsp/models/highlight.js";
 import { parseDocumentation } from "../../../util/documentationparser.js";
 
+// TODO TODO TODO
+// This function is very impure and modifies many things in the workspace.
+// Refactor this to be more functional!
 /**
  * A higher-order-function that returns a function that post-validates a component key.
  */
@@ -17,9 +21,10 @@ export function component<
 >(
     ComponentFn: new (
         ws: Workspace,
-        doc: MythicDoc,
         id: string,
-        declarations: ParsedNode[]
+        declarations: MythicComponentDeclaration<
+        Pair<ParsedNode, ParsedNode | null>
+        >[]
     ) => TComponent,
     componentType: TType,
     highlightColor: SemanticTokenTypes = SemanticTokenTypes.function,
@@ -47,6 +52,27 @@ export function component<
                     range: doc.convertToRange(key.range),
                 });
             }
+
+            const ids = ws
+                .mergedValidationResult()
+                .mythic[componentType].map((c) => c.id);
+
+            if (ids.includes(id)) {
+                result.diagnostics.push({
+                    ...DIAGNOSTIC_DEFAULT,
+                    message: `Duplicate ${componentType} id: ${id}`,
+                    range: doc.convertToRange(key.range),
+                });
+                const existingComponent = ws
+                    .mergedValidationResult()
+                    .mythic[componentType].find((c) => c.id === id);
+                if (existingComponent) {
+                    existingComponent.declarations = existingComponent.declarations.filter(
+                        (d) => d.doc !== doc,
+                    );
+                }
+            }
+
             if (!componentNode) {
                 continue;
             }
@@ -56,19 +82,37 @@ export function component<
             if (id.includes(".")) {
                 yamlRange[1] = yamlRange[0] + id.indexOf(".");
             }
-            id = id.slice(0, id.indexOf("."));
+            if (id.includes(".")) {
+                id = id.slice(0, id.indexOf("."));
+            }
             const range = doc.convertToRange(yamlRange);
             result.highlights.unshift(new Highlight(range, highlightColor));
 
             const idKey = doc.source.slice(yamlRange[0], yamlRange[1]);
-            if (walked.has(idKey)) {
+
+            ws.logger?.debug({ pair, idKey, walked, ids, ws, doc, value });
+            if (ids.includes(idKey)) {
+                const existingComponent = ws
+                    .mergedValidationResult()
+                    .mythic[componentType].find((c) => c.id === idKey);
+                existingComponent?.declarations.push({
+                    declaration: pair,
+                    doc: doc,
+                });
+            } else if (walked.has(idKey)) {
                 const component = result.mythic[componentType].find(
                     (c) => c.id === idKey,
                 );
-                component?.declarations.push(componentNode);
+                component?.declarations.push({
+                    declaration: pair,
+                    doc: doc,
+                });
             } else {
-                const newComponent: TComponent = new ComponentFn(ws, doc, id, [
-                    componentNode,
+                const newComponent: TComponent = new ComponentFn(ws, id, [
+                    {
+                        declaration: pair,
+                        doc: doc,
+                    },
                 ]);
 
                 const comment = key.commentBefore;
