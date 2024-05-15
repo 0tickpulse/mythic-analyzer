efd654457c57062dca9a9e60e1c8f4ea37353b46
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2024-05-15 14:47:20 +0800

    new declaration format for mythiccomponents

--- src/document-models/mythiccomponent.ts
+++ src/document-models/mythiccomponent.ts
@@ -1,26 +1,29 @@
-import { isMap } from "yaml";
+import { isMap, isPair } from "yaml";
 
-import type { ParsedNode } from "yaml";
+import type { Pair, ParsedNode } from "yaml";
 import type { MythicDoc } from "../doc/mythicdoc.js";
 import type { Workspace } from "../index.js";
 
-export class MythicComponent {
+class MythicComponent<T extends Pair<ParsedNode, ParsedNode | null> = Pair<ParsedNode, ParsedNode | null>> {
     public constructor(
         public ws: Workspace,
-        public doc: MythicDoc,
         public id: string,
-        public declarations: ParsedNode[],
+        public declarations: MythicComponentDeclaration<T>[],
         public documentation?: string,
     ) {
         ws.logger?.debug(`Creating MythicComponent with id ${id}, declarations ${declarations.length}. Declaration type is ${declarations[0]!.constructor.name}.`);
     }
 
     public getNode(name: string): ParsedNode | undefined {
-        return this.declarations.map(d => {
-            if (!isMap(d)) {
+        return this.declarations.map(({ declaration }) => {
+            if (!isPair(declaration)) {
                 return undefined;
             }
-            const v = d.get(name);
+            const value = declaration.value;
+            if (!isMap(value)) {
+                return undefined;
+            }
+            const v = value.get(name);
             if (!v) {
                 return undefined;
             }
@@ -28,3 +31,11 @@ export class MythicComponent {
         }).find(Boolean);
     }
 }
+
+type MythicComponentDeclaration<T extends Pair<ParsedNode, ParsedNode | null>> = {
+    declaration: T;
+    doc: MythicDoc;
+};
+
+export type { MythicComponentDeclaration };
+export { MythicComponent };
