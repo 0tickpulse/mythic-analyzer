1df097ec2c0864f6eb1f3ad76d6e1963321a87f0
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-16 15:13:07 +0800

    add the conditions logic for completions

--- src/lsp/listeners/completion.ts
+++ src/lsp/listeners/completion.ts
@@ -4,30 +4,42 @@ import type { Workspace } from "../../index.js";
 import { posIsIn } from "../../util/positions.js";
 
 export function completionHandler(workspace: Workspace) {
-    return function onCompletion(
-        params: CompletionParams,
-    ): CompletionItem[] {
-        workspace.logger?.log(`[REQUEST] Completion, ${params.textDocument.uri}`);
-        const completions = workspace.get(params.textDocument.uri)?.cachedValidationResult
-            ?.completionItems;
+    return function onCompletion(params: CompletionParams): CompletionItem[] {
+        workspace.logger?.log(
+            `[REQUEST] Completion, ${params.textDocument.uri}`,
+        );
+        const completions = workspace.get(params.textDocument.uri)
+            ?.cachedValidationResult?.completionItems;
         if (!completions) {
             workspace.logger?.log(
                 `[RESPONSE] Completion, ${params.textDocument.uri}, no completions found.`,
             );
             return [];
         }
-        const completionsInRange = completions.filter((completion) => {
+        const doc = workspace.get(params.textDocument.uri);
+        const validCompletions = completions.filter((completion) => {
             // workspace.logger?.log(
             //     `Checking if completion ${completion.items.map((item) => item.label).slice(0, 5).join(", ")}... with range ${JSON.stringify(completion.range)} is in position ${JSON.stringify(params.position)}.`,
             // );
             // const res = posIsIn(params.position, completion.range);
             // workspace.logger?.log(`Result: ${String(res)}`);
             // return res;
-            return posIsIn(params.position, completion.range);
+            return (
+                posIsIn(params.position, completion.range)
+                && ((doc
+                && completion.conditions?.(workspace, doc, params.position))
+                ?? true)
+            );
         });
         workspace.logger?.log(
-            `[RESPONSE] Completion, ${params.textDocument.uri}, ${completions.length} completions found, ${completionsInRange.length} in range.`,
+            `[RESPONSE] Completion, ${params.textDocument.uri}, ${completions.length} completions found, ${validCompletions.length} in range.`,
         );
-        return completionsInRange.flatMap((completion) => completion.items);
+        workspace.logger?.debug({
+            validCompletions: validCompletions.map((completion) => ({
+                range: completion.range,
+                items: completion.items.map((item) => item.label),
+            })),
+        });
+        return validCompletions.flatMap((completion) => completion.items);
     };
 }
